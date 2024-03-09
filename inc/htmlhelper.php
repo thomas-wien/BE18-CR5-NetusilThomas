31aa1518d439511baf8d390d648d276128c5d745
Thomas Netusil <thomas.netusil@gmail.com>
2024-03-09 12:36:26 +0100

    update style.css with darklight toggler

--- inc/htmlhelper.php
+++ inc/htmlhelper.php
@@ -16,7 +16,6 @@ function head($title = " | Thomas ")
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
       <link rel="stylesheet" type="text/css" href="../css/style.css">
-      <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
       <meta name="author" content="Thomas Netusil">
       <meta name="description" content="CodeReview 4">
       <title>Adopt a Pet' . $title . '</title>
